a6a31cf9a7bdce056f663a41102bb3e617eb795c
sallimar <sallimarxpplg1@gmail.com>
2025-01-12 16:41:14 +0700

    commit ketiga nadya

--- lib/beranda.dart
+++ lib/beranda.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pl1_kasir/main.dart';
 
 class Beranda extends StatefulWidget{
   const Beranda({super.key});
@@ -9,23 +8,99 @@ class Beranda extends StatefulWidget{
 }
 
 class _BerandaState extends State<Beranda>{
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    HomeScreen(),
+    SearchScreen(),
+    NotificationsScreen(),
+    ProfileScreen(),
+  ];
+
+  // Function to handle tab changes
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+   
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Color(0xFF2E7D32),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios,
-          size:  20,
-          color: Colors.white,
-          ),
-          onPressed: (){
-            Navigator.pop(context, MaterialPageRoute(builder: (context) => LoginPage()));
-          },
-        ),
       ),
+      body: _pages[_selectedIndex], // Display the selected page
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex, // Set the current index
+        onTap: _onItemTapped, // Handle tab taps
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Color(0xFF1B5E20)
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+            backgroundColor: Color(0xFF1B5E20)
+
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Order',
+            backgroundColor: Color(0xFF1B5E20)
+
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+            backgroundColor: Color(0xFF1B5E20)
+
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Home screen widget
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Home Screen'),
+    );
+  }
+}
+
+// Search screen widget
+class SearchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Search Screen'),
+    );
+  }
+}
+
+// Notifications screen widget
+class NotificationsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Order Screen'),
+    );
+  }
+}
+
+// Profile screen widget
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Profile Screen'),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:pl1_kasir/beranda.dart';
+import 'package:pl1_kasir/sign_up.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://mcyvjqnpfwbtztscppus.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jeXZqcW5wZndidHp0c2NwcHVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYzMDIyMDgsImV4cCI6MjA1MTg3ODIwOH0.3GIZnUXKjTRcuS5jq3eOCsIKy2KOQSnkWMr31AzfdUA',
+  );
+
   runApp(MaterialApp(
     home: LoginPage(),
     debugShowCheckedModeBanner: false,
@@ -16,8 +24,53 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final SupabaseClient supabase = Supabase.instance.client;
   bool _isPasswordVisible = false;
 
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _login() async {
+    final username = _usernameController.text.trim();
+    final password = _passwordController.text.trim();
+
+    if (username.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Harap isi semua field')),
+      );
+      return;
+    }
+
+    try {
+      final response = await supabase
+          .from('user')
+          .select('username, password')
+          .eq('username', username)
+          .single();
+
+      if (response != null && response['password'] == password) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Login berhasil')),
+        );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Beranda()),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Username atau Password salah')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Terjadi kesalahan: ${e.toString()}')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +93,7 @@ class _LoginPageState extends State<LoginPage> {
                   height: 180,
                   decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: AssetImage('assets/image/logo.png'),
+                      image: AssetImage('asset/image/logo.png'),
                     ),
                   ),
                 ),
@@ -104,9 +157,7 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     SizedBox(height: 20.0),
                     ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => Beranda()));
-                      },
+                      onPressed: _login,
                       child: Text(
                         'Login',
                         style: TextStyle(color: Colors.white),
@@ -116,6 +167,19 @@ class _LoginPageState extends State<LoginPage> {
                         backgroundColor: Color(0xFF2E7D32),
                       ),
                     ),
+                    SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Don't have an account?"),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => SignUp()));
+                          },
+                          child: const Text("SIGN UP"),
+                        )
+                      ],
+                    )
                   ],
                 ),
               ],
